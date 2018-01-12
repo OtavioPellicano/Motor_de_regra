d224625808bd71684563755e5afbb7c909ade70d
Otavio Pellicano <otavio.pellicano@gmail.com>
2018-01-12 18:02:51 -0200

    Inicio da implementacao da Regra SCM, estou comecando a fazer o layout da ABR

--- regrasscm.cpp
+++ regrasscm.cpp
@@ -2,9 +2,69 @@
 
 namespace opmm {
 
-RegrasSCM::RegrasSCM()
+RegrasSCM::RegrasSCM(const string &linhaArqCsv, const layout &tipoLayout)
 {
 
+    if(tipoLayout == ABR)
+    {
+        setDataHora(StringCsv(linhaArqCsv).getStrItemStringSplitted(1));
+        setMedidaDown(StringCsv(linhaArqCsv).getStrItemStringSplitted(29));
+        setMedidaUp(StringCsv(linhaArqCsv).getStrItemStringSplitted(32));
+        setMedidaJitter(StringCsv(linhaArqCsv).getStrItemStringSplitted(36));
+        setMedidaLatencia(StringCsv(linhaArqCsv).getStrItemStringSplitted(35));
+    }
+
+
+}
+
+std::string RegrasSCM::dataHora() const
+{
+    return mDataHora;
+}
+
+void RegrasSCM::setDataHora(const std::string &dataHora)
+{
+    mDataHora = dataHora;
+}
+
+std::string RegrasSCM::medidaDown() const
+{
+    return mMedidaDown;
+}
+
+void RegrasSCM::setMedidaDown(const std::string &medidaDown)
+{
+    mMedidaDown = medidaDown;
+}
+
+std::string RegrasSCM::medidaUp() const
+{
+    return mMedidaUp;
+}
+
+void RegrasSCM::setMedidaUp(const std::string &medidaUp)
+{
+    mMedidaUp = medidaUp;
+}
+
+std::string RegrasSCM::medidaJitter() const
+{
+    return mMedidaJitter;
+}
+
+void RegrasSCM::setMedidaJitter(const std::string &medidaJitter)
+{
+    mMedidaJitter = medidaJitter;
+}
+
+std::string RegrasSCM::medidaLatencia() const
+{
+    return mMedidaLatencia;
+}
+
+void RegrasSCM::setMedidaLatencia(const std::string &medidaLatencia)
+{
+    mMedidaLatencia = medidaLatencia;
 }
 
 
--- regrasscm.h
+++ regrasscm.h
@@ -13,6 +13,10 @@
 #include "pttnaocadastrado.h"       //#26 SCM SMP
 
 #include <string>
+#include "StringCsv.h"
+#include <vector>
+
+typedef std::vector<std::string> vecStr;
 
 namespace opmm {
 /**
@@ -21,10 +25,44 @@ namespace opmm {
  */
 class RegrasSCM
 {
+    //Funções
 public:
-    RegrasSCM();
+    RegrasSCM(const string &linhaArqCsv, const layout &tipoLayout);
+
+    //deviceid;timestamp;isp;type;manufacturer;model;software_version;ip_address;test_point;mem_total;mem_free;cpu_usage(%);os_version;os_type;battery_usage(%);cell_id;cell_id_changed;lac;tac;cgi/e-cgi;imei;imsi;network_type;signal_strength;roaming;wan_mode;network_type_changed;download_state;download_filesize_test(Bytes);download_rate(bps);upload_state;upload_filesize_test(Bytes);upload_rate(bps);udp_state;udp_estimated_triffc(Bytes);udp_latency(ms);udp_jitter(ms);udp_packet_loss_percent(%);total_traffic_sent;total_traffic_received;total_traffic;test_origin(APP|PROBE);source(Manual|Auto)
+
+
+    std::string dataHora() const;
+    void setDataHora(const std::string &dataHora);
+
+    std::string medidaDown() const;
+    void setMedidaDown(const std::string &medidaDown);
+
+    std::string medidaUp() const;
+    void setMedidaUp(const std::string &medidaUp);
+
+    std::string medidaJitter() const;
+    void setMedidaJitter(const std::string &medidaJitter);
+
+    std::string medidaLatencia() const;
+    void setMedidaLatencia(const std::string &medidaLatencia);
+
+private:
+
+
+
+    //Variaveis
+private:
+    std::string mDataHora;  //strCsv[1]
+    std::string mMedidaDown;//strCsv[29]
+    std::string mMedidaUp;  //strCsv[32]
+    std::string mMedidaJitter;//strCsv[36]
+    std::string mMedidaLatencia;//strCsv[35]
+
+
 
 };
+
 
 
 }
